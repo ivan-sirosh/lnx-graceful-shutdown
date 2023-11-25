2eeec84c0cb095227993a71d57105fb038c7981d
ivan-sirosh <36622326+ivan-sirosh@users.noreply.github.com>
2023-11-25 07:58:54 +0100

    Update remote_monitor.sh

--- remote_monitor.sh
+++ remote_monitor.sh
@@ -12,16 +12,18 @@ log() {
 
 log "Checking remote resource: $REMOTE_RESOURCE"
 
-if ping -c 1 -W 5 "$REMOTE_RESOURCE"; then
-    log "Remote resource is up."
-    # Check if shutdown flag exists, and if so, remove it
-    if [ -e "$SHUTDOWN_FLAG" ]; then
-        log "Resource is back online. Cancelling shutdown."
-        rm "$SHUTDOWN_FLAG"
-        shutdown -c  # Cancel any pending shutdown
+while true; do
+    if ping -c 1 -W 5 "$REMOTE_RESOURCE"; then
+        # Check if shutdown flag exists, and if so, remove it
+        if [ -e "$SHUTDOWN_FLAG" ]; then
+            log "Resource is back online. Cancelling shutdown."
+            rm "$SHUTDOWN_FLAG"
+            shutdown -c  # Cancel any pending shutdown
+        fi
+    else
+        log "Remote resource is down. Initiating graceful shutdown..."
+        touch "$SHUTDOWN_FLAG"  # Create a flag to indicate shutdown is in progress
+        shutdown -h "+$TIMEOUT" "Server is shutting down due to remote resource being down."
     fi
-else
-    log "Remote resource is down. Initiating graceful shutdown..."
-    touch "$SHUTDOWN_FLAG"  # Create a flag to indicate shutdown is in progress
-    shutdown -h "+$TIMEOUT" "Server is shutting down due to remote resource being down."
-fi
+    sleep 60
+done
